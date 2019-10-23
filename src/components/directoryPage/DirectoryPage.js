9962a87ffb68676df71bc8c4b13b9f6a214d88aa
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-23 15:14:26 +0100

    Movie directory with search by movie name working

--- src/components/directoryPage/DirectoryPage.js
+++ src/components/directoryPage/DirectoryPage.js
@@ -1,14 +1,41 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+import {filter, debounce} from 'lodash';
 import DirectoryHeader from './directoryHeader/DirectoryHeader';
 import DirectoryBody from './directoryBody/DirectoryBody';
 import '../directoryPage/PageStyling.css'
 
-
 const DirectoryPage = ({genresBoxNames, movieListInfo}) => {
+
+    const [searchInput, setSearchInput] = useState(null)
+    const [movieData, setMovieData] = useState(movieListInfo)
+
+    const userSearch = () => {
+
+        if (searchInput) {
+            if (typeof searchInput) {
+                const filterDataName = filter(movieListInfo, function(movie) {
+                    const inputMovieName = movie.movieName.toLowerCase()
+                    return inputMovieName.includes(searchInput)
+                })
+                setMovieData(filterDataName)
+            }
+    } else {
+        setMovieData(movieListInfo)
+    }
+}
+
+useEffect(() => {
+    userSearch()
+}, [searchInput])
+
     return (
         <>
-            <DirectoryHeader genresBoxNames={genresBoxNames}/>
-            <DirectoryBody movieListInfo={movieListInfo}/>
+            <DirectoryHeader 
+            genresBoxNames={genresBoxNames}
+            value={searchInput}
+            onChange={(e)=>setSearchInput(e.target.value)}
+            />
+            <DirectoryBody movieListInfo={movieData}/>
         </>
     )
 }
