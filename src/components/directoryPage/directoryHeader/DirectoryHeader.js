60aa7d1cf0011e38fb464e0e0c9514832d35f010
chipo mapondera <chipo.mapondera@ynap.com>
2019-10-23 11:52:24 +0100

    Movie directory with responsive styling (320, 768, 1024, 1440)

--- src/components/directoryPage/directoryHeader/DirectoryHeader.js
+++ src/components/directoryPage/directoryHeader/DirectoryHeader.js
@@ -4,13 +4,13 @@ import '../directoryHeader/HeaderStyling.css'
 const DirectoryHeader = ({genresBoxNames}) => {
     return (
         <div className="dir-header">
+            <input className="search-input" placeholder='Search here...' />
             <div className="checkbox-wrapper">
                 {genresBoxNames.map((movieGenre) => {
                     console.log('this is', movieGenre)
                     return <div className="genre-checkboxes"><input className="check-box" type="checkbox" name={movieGenre} value={movieGenre}/><span className="genre-text">{movieGenre}</span></div>
                 })}
             </div>
-            <input className="search-input" placeholder='Search here...' />
         </div> 
     )
 }
